0d6e4e245ed24f5404c01c0c904012232088f2b5
Dedes <federico.lepore73@gmail.com>
2026-02-06 11:22:38 +0100

    new repo

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import MyNav from "./components/MyNav";
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <MyNav />
+      </div>
+    </>
+  )
+}
+
+export default App
